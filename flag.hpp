8f02a053408a508ac3338567c016823eb55bde99
Ryan Keane <ryan.keane051@gmail.com>
2026-08-18 01:46:55 -0400

    Refactor flag template and improve type constraints

--- flag.hpp
+++ flag.hpp
@@ -2,19 +2,17 @@
 
 namespace rjk {
 
-template <typename Tag, bool On>
+template <typename Tag = decltype([] {}), bool On = true>
 struct flag {
-    consteval auto operator()(bool b) const {
+    consteval auto operator()(bool b) const requires On {
         return substitute(^^rjk::flag, {^^Tag, std::meta::reflect_constant(b)});
     }
 };
 
-template <typename Tag = decltype([] {})>
-consteval auto make_flag() {
-    return flag<Tag, true>{};
-}
+template <typename T>
+concept flag_type = (has_template_arguments(^^T) && template_of(^^T) == ^^flag);
 
-consteval bool is_flag_set(std::meta::info entity, auto flag) {
+consteval bool is_flag_set(std::meta::info entity, flag_type auto flag) {
     for (const auto annotation : annotations_of(entity)) {
         if (type_of(annotation) == dealias(^^std::meta::info)) {
             if (extract<std::meta::info>(annotation) == type_of(^^flag)) {
